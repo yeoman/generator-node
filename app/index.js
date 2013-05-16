e49a2738c9a23027d68d1ba9bcb8a008bd737fc2
Kyle Robinson Young <kyle@dontkry.com>
2013-05-16 12:31:47 -0700

    Filling in more defaults

--- app/index.js
+++ app/index.js
@@ -19,6 +19,7 @@ var NodeGenerator = module.exports = function NodeGenerator(args, options) {
 util.inherits(NodeGenerator, yeoman.generators.NamedBase);
 
 NodeGenerator.prototype.askFor = function askFor() {
+  var self = this;
   var cb = this.async();
 
   // welcome message
@@ -33,81 +34,85 @@ NodeGenerator.prototype.askFor = function askFor() {
   '\n   __' + '\'.___.\''.yellow + '__' +
   '\n ´   ' + '`  |'.red + '° ' + '´ Y'.red + ' `\n' +
   '\n' +
-  '_Project name_ shouldn\'t contain "node" or "js" and should ' +
-  'be a unique ID not already in use at search.npmjs.org.' +
-  '\n\n' +
-  'You should now install project dependencies with _npm ' +
-  'install_. After that, you may execute project tasks with _grunt_. For ' +
-  'more information about installing and configuring Grunt, please see ' +
-  'the Getting Started guide:' +
-  '\n\n' +
+  '_Project name_ shouldn\'t contain "node" or "js" and should\n' +
+  'be a unique ID not already in use at search.npmjs.org.\n' +
+  '\n' +
+  'You should now install project dependencies with _npm install_.\n' +
+  'After that, you may execute project tasks with _grunt_. For\n' +
+  'more information about installing and configuring Grunt, please see\n' +
+  'the Getting Started guide:\n' +
+  '\n' +
   'http://gruntjs.com/getting-started';
 
   console.log(welcome);
 
+  var projectname = path.basename(process.cwd());
+  var username = (process.env.USER || process.env.USERNAME || '???');
+
   var prompts = [{
     name: 'name',
-    message: 'Project Name:',
-    // TODO: default to folder name
+    message: 'Project name',
+    default: projectname
   }, {
     name: 'description',
-    default: 'The best project ever.'
+    default: 'The best project ever'
   }, {
     name: 'version',
     default: '0.1.0'
   }, {
     name: 'repository',
-    message: 'Project git repository'
-    // TODO: Default to git://github.com/[username]/[projectname].git
+    message: 'Project git repository',
+    default: 'git://github.com/' + username + '/' + projectname + '.git'
   }, {
     name: 'homepage',
-    message: 'Project homepage'
-    // TODO: Default to https://github.com/[username]/[projectname]
+    message: 'Project homepage',
+    default: 'https://github.com/' + username + '/' + projectname
   }, {
     name: 'bugs',
-    message: 'Project issues tracker'
-    // TODO: Default to https://github.com/[username]/[projectname]/issues
+    message: 'Project issues tracker',
+    default: 'https://github.com/' + username + '/' + projectname + '/issues'
   }, {
     name: 'license',
     default: 'MIT'
   }, {
     name: 'author_name',
-    // TODO: Default to Name
+    // TODO(shama): Default to Name
   }, {
     name: 'author_email',
-    // TODO: Default to Email
+    // TODO(shama): Default to Email
   }, {
     name: 'author_url',
-    // TODO: Default to homepage
+    // TODO(shama): Default to homepage
   }, {
     name: 'node_version',
     message: 'What versions of node does it run on?',
-    default: '>= 0.10.0'
+    default: this.pkg.engines.node
   }, {
     name: 'main',
     message: 'Main module entry point:',
-    default: 'lib/',
-    // TODO: default to project name
+    default: 'lib/' + projectname + '.js'
   }, {
     name: 'npm_test',
     message: 'npm test command:',
     default: 'grunt nodeunit',
   }, {
     name: 'travis',
-    message: 'Will this project be tested with Travis CI?'
+    message: 'Will this project be tested with Travis CI?',
+    default: 'Y/n'
   }];
 
   var nameToMessage = function (name) {
     return name.split('_').map(
-      function (x) { return this._.capitalize(x); }.bind(this)
-    ).join(' ') + ':';
-  }.bind(this);
+      function (x) { return self._.capitalize(x); }
+    ).join(' ');
+  };
 
   // Generate prompt messages if only the name is defined.
   prompts.map(function (entry) {
     if (entry.message === undefined) {
       entry.message = nameToMessage(entry.name);
     }
+    entry.message = String(entry.message).grey;
     return entry;
   });
 
@@ -115,14 +120,17 @@ NodeGenerator.prototype.askFor = function askFor() {
 
   this.prompt(prompts, function (err, props) {
     if (err) {
-      return this.emit('error', err);
+      return self.emit('error', err);
     }
-
-    this.props = props;
+    self.props = props;
     // For easier access in the templates.
-    this.slugname = this._.slugify(props.name);
+    self.slugname = self._.slugify(props.name);
     cb();
-  }.bind(this));
+  });
+};
+
+NodeGenerator.prototype.dependencies = function dependencies() {
+  // TODO(shama): look up latest devDeps and inject into package.json
 };
 
 NodeGenerator.prototype.lib = function lib() {
