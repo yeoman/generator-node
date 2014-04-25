ab49f1f622e4b79c40a1532f698f7f53db3e54c6
Jason Travis <JasonTravis@nau.edu>
2014-04-25 00:56:15 +0000

    Update to the extend inheritance method

--- app/index.js
+++ app/index.js
@@ -1,115 +1,110 @@
 'use strict';
 
 var chalk = require('chalk');
-var util = require('util');
 var path = require('path');
 var npmName = require('npm-name');
 var yeoman = require('yeoman-generator');
 
-var NodeGenerator = module.exports = function NodeGenerator(args, options) {
-  yeoman.generators.Base.apply(this, arguments);
+var NodeGenerator = module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
 
-  this.on('end', function () {
+  prompting: function () {
+    var cb = this.async();
+
+    this.log(
+      this.yeoman +
+      '\nThe name of your project shouldn\'t contain "node" or "js" and' +
+      '\nshould be a unique ID not already in use at search.npmjs.org.');
+
+    var prompts = [{
+      name: 'name',
+      message: 'Module Name',
+      default: path.basename(process.cwd()),
+      filter: function (input) {
+        var done = this.async();
+
+        npmName(input, function (err, available) {
+          if (!available) {
+            this.log.info(chalk.yellow(input) + ' already exists on npm. You might want to use another name.');
+          }
+
+          done(input);
+        });
+      }
+    }, {
+      name: 'description',
+      message: 'Description',
+      default: 'The best module ever.'
+    }, {
+      name: 'homepage',
+      message: 'Homepage'
+    }, {
+      name: 'license',
+      message: 'License',
+      default: 'MIT'
+    }, {
+      name: 'githubUsername',
+      message: 'GitHub username'
+    }, {
+      name: 'authorName',
+      message: 'Author\'s Name'
+    }, {
+      name: 'authorEmail',
+      message: 'Author\'s Email'
+    }, {
+      name: 'authorUrl',
+      message: 'Author\'s Homepage'
+    }];
+
+    this.currentYear = (new Date()).getFullYear();
+
+    this.prompt(prompts, function (props) {
+      this.slugname = this._.slugify(props.name);
+      this.safeSlugname = this.slugname.replace(
+        /-+([a-zA-Z0-9])/g,
+        function (g) { return g[1].toUpperCase(); }
+      );
+
+      if(props.githubUsername){
+        this.repoUrl = 'https://github.com/' + props.githubUsername + '/' + this.slugname;
+      } else {
+        this.repoUrl = 'user/repo';
+      }
+
+      if (!props.homepage) {
+        props.homepage = this.repoUrl;
+      }
+
+      this.props = props;
+
+      cb();
+    }.bind(this));
+  },
+
+  configuring: function () {
+    this.config.save();
+    this.copy('jshintrc', '.jshintrc');
+    this.copy('gitignore', '.gitignore');
+    this.copy('travis.yml', '.travis.yml');
+
+    this.template('_README.md', 'README.md');
+    this.template('_Gruntfile.js', 'Gruntfile.js');
+    this.template('_package.json', 'package.json');
+  },
+
+  writing: function () {
+    this.mkdir('lib');
+    this.template('lib/name.js', 'lib/' + this.slugname + '.js');
+    this.mkdir('test');
+    this.template('test/name_test.js', 'test/' + this.slugname + '_test.js');
+  },
+
+  install: function () {
     this.installDependencies({
       bower: false,
-      skipInstall: options['skip-install']
+      skipInstall: this.options['skip-install']
     });
-  });
-
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-};
-util.inherits(NodeGenerator, yeoman.generators.NamedBase);
-
-NodeGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
-  var log = this.log;
-
-  console.log(
-    this.yeoman +
-    '\nThe name of your project shouldn\'t contain "node" or "js" and' +
-    '\nshould be a unique ID not already in use at search.npmjs.org.');
-
-  var prompts = [{
-    name: 'name',
-    message: 'Module Name',
-    default: path.basename(process.cwd()),
-    filter: function (input) {
-      var done = this.async();
-
-      npmName(input, function (err, available) {
-        if (!available) {
-          log.info(chalk.yellow(input) + ' already exists on npm. You might want to use another name.');
-        }
-
-        done(input);
-      });
-    }
-  }, {
-    name: 'description',
-    message: 'Description',
-    default: 'The best module ever.'
-  }, {
-    name: 'homepage',
-    message: 'Homepage'
-  }, {
-    name: 'license',
-    message: 'License',
-    default: 'MIT'
-  }, {
-    name: 'githubUsername',
-    message: 'GitHub username'
-  }, {
-    name: 'authorName',
-    message: 'Author\'s Name'
-  }, {
-    name: 'authorEmail',
-    message: 'Author\'s Email'
-  }, {
-    name: 'authorUrl',
-    message: 'Author\'s Homepage'
-  }];
-
-  this.currentYear = (new Date()).getFullYear();
-
-  this.prompt(prompts, function (props) {
-    this.slugname = this._.slugify(props.name);
-    this.safeSlugname = this.slugname.replace(
-      /-+([a-zA-Z0-9])/g,
-      function (g) { return g[1].toUpperCase(); }
-    );
-
-    if(props.githubUsername){
-      this.repoUrl = 'https://github.com/' + props.githubUsername + '/' + this.slugname;
-    } else {
-      this.repoUrl = 'user/repo';
-    }
-
-    if (!props.homepage) {
-      props.homepage = this.repoUrl;
-    }
-
-    this.props = props;
-
-    cb();
-  }.bind(this));
-};
-
-NodeGenerator.prototype.lib = function lib() {
-  this.mkdir('lib');
-  this.template('lib/name.js', 'lib/' + this.slugname + '.js');
-};
-
-NodeGenerator.prototype.test = function test() {
-  this.mkdir('test');
-  this.template('test/name_test.js', 'test/' + this.slugname + '_test.js');
-};
-
-NodeGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('jshintrc', '.jshintrc');
-  this.copy('gitignore', '.gitignore');
-  this.copy('travis.yml', '.travis.yml');
-
-  this.template('README.md');
-  this.template('Gruntfile.js');
-  this.template('_package.json', 'package.json');
-};
+  }
+});
