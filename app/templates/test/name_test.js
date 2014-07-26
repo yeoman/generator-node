b1d268cb4cec31b904f87a2f89c8a6c4d965c1dc
hemanth.hm <hemanth.hm@gmail.com>
2014-07-26 10:20:20 +0530

    Migrated to mocha.

--- app/templates/test/name_test.js
+++ app/templates/test/name_test.js
@@ -1,36 +1,10 @@
+/*global describe,it*/
 'use strict';
+var assert = require('assert'),
+  <%= safeSlugname %> = require('../lib/<%= slugname %>.js');
 
-var <%= safeSlugname %> = require('../lib/<%= slugname %>.js');
-
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
-exports.<%= safeSlugname %> = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(<%= safeSlugname %>.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  }
-};
+describe('<%= slugname %> node module.', function() {
+  it('must be awesome', function() {
+    assert( <%= safeSlugname %> .awesome(), 'awesome');
+  });
+});
