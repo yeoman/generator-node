709a9143c072be19d9e5b74b156e9fda85b2173b
Daniel Furze <daniel@furzeface.com>
2014-12-02 20:29:46 +0000

    Add failing test example to scaffolded tests

--- app/templates/test/name_test.js
+++ app/templates/test/name_test.js
@@ -3,8 +3,14 @@
 var assert = require('assert'),
   <%= safeSlugname %> = require('../lib/<%= slugname %>.js');
 
-describe('<%= slugname %> node module.', function() {
+describe('<%= slugname %> node module', function() {
   it('must be awesome', function() {
     assert.equal( <%= safeSlugname %>.awesome(), 'awesome');
+  });
+});
+
+describe('<%= slugname %> node module', function() {
+  it('must have at least one test', function() {
+    assert(false, 'I was too lazy to write any tests. Shame on me.');
   });
 });
