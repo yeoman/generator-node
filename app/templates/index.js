d47a7ada95164094957f483edf072985b0af877a
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-12-07 15:29:16 +0100

    Fix indentation

--- app/templates/index.js
+++ app/templates/index.js
@@ -1,4 +1,4 @@
 'use strict';
 module.exports = function (str) {
-    console.log(str || 'Rainbow');
+  console.log(str || 'Rainbow');
 };
--- app/templates/test/test.js
+++ app/templates/test/test.js
@@ -4,8 +4,8 @@ var assert = require('assert');
 var <%= safeSlugname %> = require('../');
 
 describe('<%= slugname %> node module', function () {
-    it('must have at least one test', function () {
-        <%= safeSlugname %>();
-        assert(false, 'I was too lazy to write any tests. Shame on me.');
-    });
+  it('must have at least one test', function () {
+    <%= safeSlugname %>();
+    assert(false, 'I was too lazy to write any tests. Shame on me.');
+  });
 });
