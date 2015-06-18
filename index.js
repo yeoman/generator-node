9ca495c77ce163618710af131f98d64a5457f768
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-18 09:02:18 +0200

    Remove unneeded quotes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  'app': require.resolve('./generators/app'),
-  'boilerplate': require.resolve('./generators/boilerplate'),
-  'editorconfig': require.resolve('./generators/editorconfig'),
-  'git': require.resolve('./generators/git'),
-  'gulp': require.resolve('./generators/gulp'),
-  'jscs': require.resolve('./generators/jscs'),
-  'jshint': require.resolve('./generators/jshint'),
-  'readme': require.resolve('./generators/readme'),
-  'travis': require.resolve('./generators/travis')
+  app: require.resolve('./generators/app'),
+  boilerplate: require.resolve('./generators/boilerplate'),
+  editorconfig: require.resolve('./generators/editorconfig'),
+  git: require.resolve('./generators/git'),
+  gulp: require.resolve('./generators/gulp'),
+  jscs: require.resolve('./generators/jscs'),
+  jshint: require.resolve('./generators/jshint'),
+  readme: require.resolve('./generators/readme'),
+  travis: require.resolve('./generators/travis')
 };
