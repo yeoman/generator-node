e6b927e69bbcb102edfdb42be8fbc3ed65c232eb
Simon Boudrias <admin@simonboudrias.com>
2015-07-10 19:56:50 -0700

    Define options in node:cli

--- generators/cli/index.js
+++ generators/cli/index.js
@@ -3,6 +3,23 @@ var _ = require('lodash');
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+
+    this.option('name', {
+      type: String,
+      required: true,
+      desc: 'The module name'
+    });
+
+    this.option('babel', {
+      type: Boolean,
+      required: false,
+      defaults: false,
+      desc: 'pre-compile with Babel'
+    });
+  },
+
   writing: {
     package: function () {
       var pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
