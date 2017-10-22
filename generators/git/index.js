d2f58375fb4b8cac48cfeace68a023ff94486865
joelchucn <joelchu@foxmail.com>
2017-10-22 09:03:56 +0100

    change the generator/git/ into ES6 class

--- generators/git/index.js
+++ generators/git/index.js
@@ -2,85 +2,84 @@
 const Generator = require('yeoman-generator');
 const originUrl = require('git-remote-origin-url');
 
-module.exports = Generator.extend({
-  constructor: function() {
-    Generator.apply(this, arguments);
+module.exports = class extends Generator {
 
-    this.option('generateInto', {
-      type: String,
-      required: false,
-      defaults: '',
-      desc: 'Relocate the location of the generated files.'
-    });
+    constructor(args, opts) {
+        super(args, opts);
+        this.option('generateInto', {
+          type: String,
+          required: false,
+          defaults: '',
+          desc: this.t('generateInto')
+        });
 
-    this.option('name', {
-      type: String,
-      required: true,
-      desc: 'Module name'
-    });
+        this.option('name', {
+          type: String,
+          required: true,
+          desc: this.t('moduleName')
+        });
 
-    this.option('github-account', {
-      type: String,
-      required: true,
-      desc: 'GitHub username or organization'
-    });
-  },
+        this.option('github-account', {
+          type: String,
+          required: true,
+          desc: this.t('githubAccount')
+        });
+    }
+    initializing() {
+      this.fs.copy(
+        this.templatePath('gitattributes'),
+        this.destinationPath(this.options.generateInto, '.gitattributes')
+      );
 
-  initializing: function() {
-    this.fs.copy(
-      this.templatePath('gitattributes'),
-      this.destinationPath(this.options.generateInto, '.gitattributes')
-    );
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath(this.options.generateInto, '.gitignore')
+      );
 
-    this.fs.copy(
-      this.templatePath('gitignore'),
-      this.destinationPath(this.options.generateInto, '.gitignore')
-    );
-
-    return originUrl(this.destinationPath(this.options.generateInto)).then(
-      function(url) {
-        this.originUrl = url;
-      }.bind(this),
-      function() {
-        this.originUrl = '';
-      }.bind(this)
-    );
-  },
-
-  writing: function() {
-    this.pkg = this.fs.readJSON(
-      this.destinationPath(this.options.generateInto, 'package.json'),
-      {}
-    );
-
-    let repository = '';
-    if (this.originUrl) {
-      repository = this.originUrl;
-    } else {
-      repository = this.options.githubAccount + '/' + this.options.name;
+      return originUrl(this.destinationPath(this.options.generateInto)).then(
+        function(url) {
+          this.originUrl = url;
+        }.bind(this),
+        function() {
+          this.originUrl = '';
+        }.bind(this)
+      );
     }
 
-    this.pkg.repository = this.pkg.repository || repository;
+    writing() {
+      this.pkg = this.fs.readJSON(
+        this.destinationPath(this.options.generateInto, 'package.json'),
+        {}
+      );
 
-    this.fs.writeJSON(
-      this.destinationPath(this.options.generateInto, 'package.json'),
-      this.pkg
-    );
-  },
-
-  end: function() {
-    this.spawnCommandSync('git', ['init', '--quiet'], {
-      cwd: this.destinationPath(this.options.generateInto)
-    });
-
-    if (!this.originUrl) {
-      let repoSSH = this.pkg.repository;
-      if (this.pkg.repository && this.pkg.repository.indexOf('.git') === -1) {
-        repoSSH = 'git@github.com:' + this.pkg.repository + '.git';
+      let repository = '';
+      if (this.originUrl) {
+        repository = this.originUrl;
+      } else {
+        repository = this.options.githubAccount + '/' + this.options.name;
       }
-      this.spawnCommandSync('git', ['remote', 'add', 'origin', repoSSH], {
+
+      this.pkg.repository = this.pkg.repository || repository;
+
+      this.fs.writeJSON(
+        this.destinationPath(this.options.generateInto, 'package.json'),
+        this.pkg
+      );
+    }
+
+    end() {
+      this.spawnCommandSync('git', ['init', '--quiet'], {
         cwd: this.destinationPath(this.options.generateInto)
       });
+
+      if (!this.originUrl) {
+        let repoSSH = this.pkg.repository;
+        if (this.pkg.repository && this.pkg.repository.indexOf('.git') === -1) {
+          repoSSH = 'git@github.com:' + this.pkg.repository + '.git';
+        }
+        this.spawnCommandSync('git', ['remote', 'add', 'origin', repoSSH], {
+          cwd: this.destinationPath(this.options.generateInto)
+        });
+      }
     }
-  }
-});
+}
